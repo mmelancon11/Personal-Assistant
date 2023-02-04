06dfe26204d97e4285beee89ba0a464d5a207b76
mmelancon11 <103615491+mmelancon11@users.noreply.github.com>
2023-02-04 11:32:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
-import speech_recognition as sr
-import pyttsx3 as tts
+import datetime
+
+from bfunctions import take_command, talk
 from alarm import alarm
+import pyttsx3 as tts
 
-listener = sr.Recognizer()
+
 engine = tts.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
-
-
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('Listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
-    return command
 
 
 def run():
     command = take_command()
-    print(command)
+    running = True
+    if 'set an alarm at' in command:
+        time = command[-5:]
+        print(time)
+        alarm(time)
+    elif 'what is the time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+    elif 'goodbye' in command:
+        talk('Goodbye Max')
+        running = False
+    else:
+        talk("I didn't quite get that")
+    return running
 
 
-alarm_time = input('>')
-
-alarm(alarm_time)
+while run():
+    try:
+        run()
+    except:
+        pass
